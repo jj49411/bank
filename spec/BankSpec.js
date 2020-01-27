459799aeacda2bce087a097b483925f902b76429
Julie <j49411@gmail.com>
2020-01-27 12:08:15 +0000

    Create bank class and deposit function

---
+++ spec/BankSpec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('Bank', function() {
+  var bank;
+
+  it('can deposit money in an account', function() {
+    bank = new Bank;
+    expect(bank.deposit(30)).toBe(30)
+  });
+});
---
+++ src/Bank.js
@@ -0,0 +1,8 @@
+'use strict';
+
+class Bank {
+
+  deposit(amount) {
+    return amount;
+  }
+}
