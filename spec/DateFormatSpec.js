6d8123b8fd6f5247c87ca69aad22e6553c6b5387
Julie <j49411@gmail.com>
2020-01-27 21:36:10 +0000

    Create dateFormat class and function to return today's date

---
+++ spec/DateFormatSpec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+describe('DateFormat', function() {
+  var today;
+
+  beforeEach(function() {
+    today = new Date(2020,1,14)
+    jasmine.clock().install();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
+  it('should return the date of today', function() {
+    jasmine.clock().mockDate(today);    
+    var date = new DateFormat;
+    expect(date.dateFormat()).toEqual('14/02/2020')
+  });
+  
+});
---
+++ src/DateFormat.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class DateFormat {
+
+  dateFormat() {
+    var date = new Date;
+    var day = date.getDate().toString();
+    var month = (date.getMonth() + 1).toString();
+    var year = date.getFullYear().toString();
+    if(month.length == 1) {
+      month = "0" + month;
+    }
+    if(day.length == 1) {
+      day = "0" + day;
+    }
+
+    return day + '/' + month + '/' + year
+  }
+
+}
