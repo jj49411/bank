e856f45b866e3469c5d00971e5cb1f2de9b714c9
Julie <j49411@gmail.com>
2020-01-27 14:31:31 +0000

    Install Karma for test coverage

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -3,14 +3,20 @@
 describe('Bank', function() {
   var bank;
 
-  it('can deposit money in an account', function() {
+  beforeEach(function() {
     bank = new Bank;
+  });
+
+  it('can deposit money in an account', function() {
     expect(bank.deposit(30)).toBe(30)
   });
 
   it('can add add on value in the account when deposit money', function() {
-    bank = new Bank;
     bank.deposit(40);
     expect(bank.deposit(50)).toBe(90)
+  })
+
+  it('can record the time when depositing money', function() {
+    expect(bank.deposit(100)).to
   })
 });
