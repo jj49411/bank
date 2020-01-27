292386527487dd3c6ee38a156fd593e6b0efe4b4
Julie <j49411@gmail.com>
2020-01-27 22:27:16 +0000

    Update deposit function to add a transaction

--- src/Account.js
+++ src/Account.js
@@ -2,13 +2,16 @@
 
 class Account {
 
-  constructor(balance = 0) {
+  constructor(balance = 0, transactions =[]) {
     this.balance = balance;
+    this.transactions = transactions;
   }
  
   deposit(amount) {
     var transaction = new Transaction;
+    var date = new DateFormat;
     this.balance += transaction.add(amount);
+    this.transactions.push([date.dateFormat(), amount, this.balance]);
   }
 
   withdraw(amount) {
